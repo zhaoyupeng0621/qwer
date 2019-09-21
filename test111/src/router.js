b2da1b5010938461951b9892330c89ce210c472f
zhaoyupeng <2051069854@qq.com>
2019-09-21 14:47:35 +0800

    qq

--- test111/src/router.js
+++ test111/src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Test from './views/Test'
+import Test1 from './views/Test1'
+import Test2 from './views/Test2'
 
 Vue.use(Router)
 
@@ -20,6 +23,23 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/test',
+      name: 'about',
+      component:Test,
+      children:[
+        {
+          path:'test1',
+          name:'test1',
+          component:Test1
+        },
+        {
+          path:'test2',
+          name:'test2',
+          component:Test2 
+        }
+      ]
     }
   ]
 })
---
+++ test111/src/views/Test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        sdasadsaddasdas
+        <router-view></router-view>
+        <router-link to="/test/test1">test1</router-link>|
+        <router-link to="/test/test2">test2</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
